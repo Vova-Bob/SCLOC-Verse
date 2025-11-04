b95ab5b62f55cf075756a22d6b86e45fc071ea5d
AlexLiberty <aelenok@gmail.com>
2025-11-04 20:05:40 +0100

    #Fix Додав інтерфейс Updater

---
+++ StarCitizenUA/Interfaces/IUpdater.cs
@@ -0,0 +1,15 @@
+﻿using StarCitizenUA.Models.LiaModels;
+
+namespace StarCitizenUA.Interfaces
+{
+    public interface IUpdater
+    {
+        Task<Dictionary<string, string>> GetRemoteFileListAsync();
+        Task<SyncResult> SyncFilesAsync(
+            Dictionary<string, string> remoteFiles,
+            string localPath,
+            Action<string>? onProgress = null,
+            IProgress<int>? progress = null);
+        Task<bool> DownloadAndInstallVoskModelAsync(string baseFolder, Action<string>? onProgress = null);
+    }
+}
