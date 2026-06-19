532268314fe743f37edf56da03828bf2eca285b9
sclocbot-windows <sclocbot@windows.local>
2026-06-19 23:50:57 +0300

    P0 Task 20: Інтеграція UI автооновлення

--- StarCitizenUA/Composition/AppCompositionRoot.cs
+++ StarCitizenUA/Composition/AppCompositionRoot.cs
@@ -1,10 +1,12 @@
 using StarCitizenUA.Helpers;
 using StarCitizenUA.Interfaces;
 using StarCitizenUA.Services;
+using StarCitizenUA.Services.ApplicationUpdate;
 using StarCitizenUA.Services.Common;
 using StarCitizenUA.Services.LiaServices;
-using StarCitizenUA.ViewModels;
 using StarCitizenUA.Services.LocalizationServices;
+using StarCitizenUA.ViewModels;
+using System.Net.Http;
 
 
 namespace StarCitizenUA.Composition
@@ -16,6 +18,9 @@ namespace StarCitizenUA.Composition
         private readonly ISettingsService _settingsService;
         private readonly IUpdater _updater;
         private readonly UpdateCheckerService _updateCheckerService;
+        private readonly IApplicationVersionProvider _applicationVersionProvider;
+        private readonly IUpdateChannelService _updateChannelService;
+        private readonly IApplicationUpdateService _applicationUpdateService;
 
         public AppCompositionRoot()
         {
@@ -24,6 +29,23 @@ namespace StarCitizenUA.Composition
             _settingsService = new SettingsService();
             _updater = new Updater();
             _updateCheckerService = new UpdateCheckerService(_updater);
+
+            _applicationVersionProvider = new ApplicationVersionProvider();
+            _updateChannelService = (IUpdateChannelService)_settingsService;
+
+            var httpClient = new HttpClient();
+            var gitHubClient = new GitHubReleaseClient(httpClient, UpdateConstants.UserAgent);
+            var updateCacheService = new UpdateCacheService();
+            var releaseChannelResolver = new ReleaseChannelResolver();
+
+            _applicationUpdateService = new ApplicationUpdateService(
+                "Vova-Bob",
+                "SCLocalizationUA",
+                _applicationVersionProvider,
+                _updateChannelService,
+                gitHubClient,
+                updateCacheService,
+                releaseChannelResolver);
         }
 
         public MainWindow CreateMainWindow()
@@ -35,7 +57,16 @@ namespace StarCitizenUA.Composition
             var localizationInstaller = new LocalizationInstaller();
             var readmeService = new ReadmeService();
 
-            return new MainWindow(viewModel, windowHelper, localizationInstaller, readmeService, _updater, _updateCheckerService);
+            return new MainWindow(
+                viewModel,
+                windowHelper,
+                localizationInstaller,
+                readmeService,
+                _updater,
+                _updateCheckerService,
+                _applicationUpdateService,
+                _updateChannelService,
+                _applicationVersionProvider);
         }
     }
 }
