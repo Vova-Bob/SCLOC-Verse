f2841fd63ace81d87c2356d92bcc4c08999aa847
sclocbot-windows <sclocbot@windows.local>
2026-06-19 19:43:13 +0300

    P0 Task 10: Фінальна реалізація IUpdateCacheService та UpdateCacheService з TTL в IsValid

--- StarCitizenUA/Interfaces/IUpdateCacheService.cs
+++ StarCitizenUA/Interfaces/IUpdateCacheService.cs
@@ -1,4 +1,5 @@
 using StarCitizenUA.Models.ApplicationUpdate;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace StarCitizenUA.Interfaces
     {
         Task<UpdateCacheEntry?> ReadAsync(UpdateChannel channel);
         Task WriteAsync(UpdateChannel channel, List<GitHubRelease> releases);
-        bool IsValid(UpdateCacheEntry? entry);
+        bool IsValid(UpdateCacheEntry? entry, TimeSpan ttl);
         Task ClearAsync();
     }
 }
